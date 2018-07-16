94cb81fdf8db05e32c4bd0f0168f77d93e4a0a9f
ereng <aereng@gmail.com>
2018-07-16 13:20:02 +0300

    use third party login in the main app

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,5 @@
 <?php
-Route::post('/api/auth/accesstoken', 'App\Http\Controllers\Auth\APIController@login')->name('login');
-
-Route::group(['prefix' => 'api','middleware' => 'auth:api'], function () {
+Route::group(['prefix' => 'api','middleware' => 'auth:tpa_api'], function () {
 	Route::get('/testmenu', 'ILabAfrica\EMRInterface\EMR@testmenu');
 	Route::post('/testrequest', 'ILabAfrica\EMRInterface\EMR@receiveTestRequest');
 });
